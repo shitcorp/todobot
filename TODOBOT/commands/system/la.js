fadc7217b77c327c8782214a0778e74d222db40d
-view <b.buhles@gmail.com>
2020-06-21 20:36:14 +0200

    a

--- TODOBOT/commands/system/la.js
+++ TODOBOT/commands/system/la.js
@@ -5,13 +5,14 @@ const dateFormat = require('dateformat');
 
 exports.run = async (client, message, args, level) => {
 
-    message.channel.send(`Update worked`)
+    
     
     const friendly = client.config.permLevels.find(l => l.level === level).name;
 
     let embed = new Discord.RichEmbed()
         .setAuthor(`${message.author.tag}`, message.author.avatarURL)
         .setDescription(`Your permission level is: \n> **${friendly}**.`)
+        .addField("update", "worked")
         .setColor("#2C2F33")
     
     message.channel.send(embed).then(msg => { msg.delete(client.config.msgdelete).catch(error => {}) })
