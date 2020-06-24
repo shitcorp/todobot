557f9eaf3add3c253b3f3bfe8ec427c5f1ba3748
-view <b.buhles@gmail.com>
2020-06-25 01:06:14 +0200

    added dynamic activity

--- TODOBOT/events/ready.js
+++ TODOBOT/events/ready.js
@@ -1,9 +1,25 @@
+var cron = require('node-cron');
+
 module.exports = async client => {
-    // Log that the bot is online.
+ 
+  // Log that the bot is online.
     client.logger.log(`${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
       // Make the bot "play the game" which is the help command with default prefix.
     client.user.setActivity(`//help  || invite.todo-bot.xyz`, { type: 3, browser: "DISCORD IOS"  });
   
+  
+    cron.schedule('*/7 * * * *', () => {
+      let stati = [
+        '//help  || invite.todo-bot.xyz',
+        'Use //suggest to suggest new features!',
+        'Use //support to join the official support server',
+        'Bot is updating in roughly 3 - 4 days, just so that you know. Since I`m moving databases there could be some data loss!'
+      ]
+      const randomstatus = stati[Math.floor(Math.random()*stati.length)]
+      client.user.setActivity(randomstatus, { type: 3, browser: "DISCORD IOS"  });
+    });
+ 
+ 
     /*
     !Here we check if the database exists, if not were creating it.
     !This is important cause it will create this database if the bot
