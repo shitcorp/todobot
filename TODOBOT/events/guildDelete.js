dec4567b8e982de9d0eb5c04c1ecf339969b404a
-view <b.buhles@gmail.com>
2020-08-10 07:16:25 +0200

    forgot .cache in guildDelete event

--- TODOBOT/events/guildDelete.js
+++ TODOBOT/events/guildDelete.js
@@ -1,7 +1,7 @@
 module.exports = (client, guild) => {
 
     const { RichEmbed } = require('discord.js')
-    let G = client.guilds.get("710022036252262485").channels.get("724031336351793263")
+    let G = client.guilds.cache.get("710022036252262485").channels.cache.get("724031336351793263")
     let newserv = new RichEmbed()
     .setTitle(`Bot has been removed from a guild.`)
     .setThumbnail(guild.iconURL)
