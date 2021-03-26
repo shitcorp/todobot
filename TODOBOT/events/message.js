0f8f7d2b80fee733af1474b5ac756b1f40434fb2
MeerBiene <60227302+MeerBiene@users.noreply.github.com>
2021-03-26 20:30:24 +0100

    :sparkles: (help-cmd) add "normal" help command back
    This command aims to help users who still have the bot on their servers and find themselves in the situation where the bot just simply doesnt work or doesnt
    relpy whatsoever
    
    Closes #17

--- TODOBOT/events/message.js
+++ TODOBOT/events/message.js
@@ -1,9 +1,7 @@
 const cmdRecently = new Set();
 
-
 module.exports = async (client, message) => {
-
-  const timeout = process.env.MSG_DELETE || 90000
+  const timeout = process.env.MSG_DELETE || 90000;
 
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
@@ -11,30 +9,35 @@ module.exports = async (client, message) => {
   let Prefix;
   let settings;
 
-  message.guild ? settings = await client.getconfig(message.guild.id) : null;
+  message.guild ? (settings = await client.getconfig(message.guild.id)) : null;
 
-  settings
-    ? Prefix = settings.prefix
-    : Prefix = "//";
+  settings ? (Prefix = settings.prefix) : (Prefix = "//");
 
-  if (settings && settings.autopurge === true && message.channel.id === settings.todochannel) 
-    return message.delete().catch(e => client.logger.debug(e));
+  // autopurge
+  if (
+    settings &&
+    settings.autopurge === true &&
+    message.channel.id === settings.todochannel
+  )
+    return message.delete().catch((e) => client.logger.debug(e));
 
   const prefixMention = new RegExp(`^<@!?${client.user.id}>( |)$`);
   if (message.content.match(prefixMention)) {
-    return message.reply(client.embed(`My prefix on this guild is ${Prefix}`))
-      .then(msg => { msg.delete({ timeout }).catch(e => client.logger.debug(e)) });
+    return message
+      .reply(client.embed(`My prefix on this guild is ${Prefix}`))
+      .then((msg) => {
+        msg.delete({ timeout }).catch((e) => client.logger.debug(e));
+      });
   }
-
 
   if (message.content.indexOf(Prefix) !== 0) return;
 
-
-
-  const args = message.content.slice(Prefix.length).trim().split(/ +/g);
+  const args = message.content
+    .slice(Prefix.length)
+    .trim()
+    .split(/ +/g);
 
   const command = args.shift().toLowerCase();
-
 
   /**
    *  Begin Taghandler
@@ -43,15 +46,20 @@ module.exports = async (client, message) => {
   // convert settings.tag object into map
 
   if (settings) {
-
     // Check for blacklisted users here
-    if (settings.blacklist_users && Object.values(settings.blacklist_users).includes(message.author.id)) return;
-
+    if (
+      settings.blacklist_users &&
+      Object.values(settings.blacklist_users).includes(message.author.id)
+    )
+      return;
 
     // Check if the message is in a blacklisted channel
-    if (settings.blacklist_channels && Object.values(settings.blacklist_channels).includes(message.channel.id)) return;
+    if (
+      settings.blacklist_channels &&
+      Object.values(settings.blacklist_channels).includes(message.channel.id)
+    )
+      return;
 
-    console.log(settings);
     /**
      * Start Taghandler
      */
@@ -61,22 +69,23 @@ module.exports = async (client, message) => {
     /**
      *  End Taghandler
      */
-
   }
 
+  if (message.guild && !message.member)
+    await message.guild.fetchMember(message.author);
 
-  if (message.guild && !message.member) await message.guild.fetchMember(message.author);
-
-
-  const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
+  const cmd =
+    client.commands.get(command) ||
+    client.commands.get(client.aliases.get(command));
 
   if (!cmd) return;
 
-
   if (cmd && !message.guild && cmd.conf.guildOnly)
-    return message.channel.send(client.warning("This command is unavailable via private message. Please run this command in a guild."));
-
-
+    return message.channel.send(
+      client.warning(
+        "This command is unavailable via private message. Please run this command in a guild."
+      )
+    );
 
   message.flags = [];
   for (const index in args) {
@@ -87,32 +96,42 @@ module.exports = async (client, message) => {
 
   message.persists = [];
   for (const index in args) {
-    if (args[index].startsWith("~")) message.persists.push(args.shift().slice(1));
+    if (args[index].startsWith("~"))
+      message.persists.push(args.shift().slice(1));
   }
 
   if (message.flags.includes("h") || message.flags.includes("help")) {
-    let helpcmd = client.commands.get("help")
-    let arg = [command]
-    return helpcmd.run(client, message, arg, level)
+    let helpcmd = client.commands.get("help");
+    let arg = [command];
+    return helpcmd.run(client, message, arg, level);
   }
 
   // global cooldown here
   if (cmdRecently.has(message.author.id)) {
-    return message.reply(client.warning(`Please wait  \`${client.cooldown / 1000}\`  seconds before doing this command again!`)).then(msg => {
-      if (msg.deletable) msg.delete({ timeout })
-    })
+    return message
+      .reply(
+        client.warning(
+          `Please wait  \`${client.cooldown /
+            1000}\`  seconds before doing this command again!`
+        )
+      )
+      .then((msg) => {
+        if (msg.deletable) msg.delete({ timeout });
+      });
   } else {
-    cmdRecently.add(message.author.id)
+    cmdRecently.add(message.author.id);
     setTimeout(() => {
-      cmdRecently.delete(message.author.id)
-    }, client.cooldown)
+      cmdRecently.delete(message.author.id);
+    }, client.cooldown);
 
-    client.logger.cmd(`[CMD] ${message.author.username} (ID: ${message.author.id}) ran the command '${cmd.help.name}', in the guild '${message.guild.name}' (ID: ${message.guild.id})`);
+    client.logger.cmd(
+      `[CMD] ${message.author.username} (ID: ${message.author.id}) ran the command '${cmd.help.name}', in the guild '${message.guild.name}' (ID: ${message.guild.id})`
+    );
     try {
+      let level = 1;
       cmd.run(client, message, args, level);
     } catch (e) {
       client.logger.debug(e);
     }
   }
-
-}
+};
