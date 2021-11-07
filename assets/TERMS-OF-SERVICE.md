921bd6ed7284cc353a90fcb6288085b35b97e7ad
MeerBiene <60227302+MeerBiene@users.noreply.github.com>
2021-11-07 17:46:05 +0100

    initial commit

--- assets/TERMS-OF-SERVICE.md
+++
@@ -1,3 +0,0 @@
-# Terms of Service
-
-By using this product you agree to tell all your friends about it.
--- repo/shared/src/models/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Todomodel } from './todomodel'
-export { default as Configmodel } from './configmodel'
-export { default as Remindermodel } from './remindermodel'
